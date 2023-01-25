4449d55d713b7a3e22a551134d5e61acd2e998aa
gjonesdev <mail@gjones.design>
2023-01-24 19:07:31 -0800

    updated for use on reddit new

--- scripts/reddit.js
+++ scripts/reddit.js
@@ -3,43 +3,43 @@
 
 
 function toggle_post(value) {
-    // Remove margin on individual posts.
-    // Fixes issues on /r/outoftheloop
-    var posts = document.getElementsByClassName("thing");
+	// Remove margin on individual posts.
+	// Fixes issues on /r/outoftheloop
+	var posts = document.getElementsByClassName("thing");
 
-    for (var i=0; i<posts.length; i++) {
-        posts[i].style.marginRight = value;
-    }
+	for (var i = 0; i < posts.length; i++) {
+		posts[i].style.marginRight = value;
+	}
 }
 
 
 function main() {
-    var sidebar = document.getElementsByClassName("side")[0];
-    var content = document.querySelector("[role=\"main\"]");
+	var sidebar = document.getElementsByClassName("_3Kd8DQpBIbsr5E1JcrMFTY _1tvThPWQpORoc2taKebHxs")[0];
+	var content = document.querySelector("[role=\"main\"]");
 
-    if (sidebar) {
-        var display = sidebar.style.display;
+	if (sidebar) {
+		var display = sidebar.style.display;
 
-        if (display != "none") {
-            sidebar.style.display = "none";
-            toggle_post("15px");
+		if (display != "none") {
+			sidebar.style.display = "none";
+			toggle_post("15px");
 
-            // !important needs to be used here since this class
-            // can't be overidden otherwise.
-            content.style.cssText = `margin-top: 20px !important;
+			// !important needs to be used here since this class
+			// can't be overidden otherwise.
+			content.style.cssText = `margin-top: 20px !important;
                                      margin-right: 15px !important;
                                      padding-top: 0px !important`;
-            content.style.border  = "none";
+			content.style.border = "none";
 
-        } else {
-            // Reset styles.
-            sidebar.style.display = null;
-            content.style.border  = null;
-            content.style.margin  = null;
-            content.style.padding = null;
-            toggle_post(null);
-        }
-    }
+		} else {
+			// Reset styles.
+			sidebar.style.display = null;
+			content.style.border = null;
+			content.style.margin = null;
+			content.style.padding = null;
+			toggle_post(null);
+		}
+	}
 }
 
 
